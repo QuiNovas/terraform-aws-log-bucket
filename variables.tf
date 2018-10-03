d87890eaab3669a8b7b9c25fa56a50737c668371
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-10-03 13:51:01 -0500

    Added server-side encryption
    and made the glacier and expiration variables, and made the lifecycle rule apply to the whole bucket

--- variables.tf
+++ variables.tf
@@ -1,4 +1,16 @@
+variable "expiration" {
+  default     = 2555
+  description = "The number of days to wait before expiring an object"
+  type        = "string"
+}
+
 variable "name_prefix" {
   description = "The name prefix to use when creating resource names"
+  type        = "string"
+}
+
+variable "transition_to_glacier" {
+  default     = 30
+  description = "The number of days to wait before transitioning an object to Glacier"
   type        = "string"
 }
