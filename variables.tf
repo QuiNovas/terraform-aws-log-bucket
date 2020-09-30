2ddb0dac76543ac3a9a439dec102bb230b0d9852
Kumar Rajhansh <krajhansh@quinovas.com>
2020-09-30 18:38:15 +0530

    added public access resource

--- variables.tf
+++ variables.tf
@@ -14,3 +14,27 @@ variable "transition_to_glacier" {
   description = "The number of days to wait before transitioning an object to Glacier"
   type        = number
 }
+
+variable "block_public_acls" {
+  default     = true
+  description = "Whether Amazon S3 should block public ACLs for this bucket."
+  type        = bool
+}
+
+variable "block_public_policy" {
+  default     = true
+  description = "Whether Amazon S3 should block public bucket policies for this bucket."
+  type        = bool
+}
+
+variable "ignore_public_acls" {
+  default     = true
+  description = "Whether Amazon S3 should ignore public ACLs for this bucket"
+  type        = bool
+}
+
+variable "restrict_public_buckets" {
+  default     = true
+  description = "Whether Amazon S3 should restrict public bucket policies for this bucket."
+  type        = bool
+}
